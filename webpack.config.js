6ae255774dfb17426f2c45ba5d9bc97cc3da6c92
Michael Y Kim <michaelkim2786@gmail.com>
2020-09-22 18:10:38 -0400

    error message

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     context: __dirname,
     entry: './frontend/index.jsx',
     output: {
-        path: 'app/assets/javascripts',
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
     },
     resolve: {
