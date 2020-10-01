41868cbb43176f42b432406d44248a4e48010b95
Michael Y Kim <michaelkim2786@gmail.com>
2020-10-01 14:01:06 -0400

    google maps API work

--- frontend/components/housing/housing_index.jsx
+++ frontend/components/housing/housing_index.jsx
@@ -16,19 +16,22 @@ class HousingIndex extends React.Component {
 
     render() {   
     return (
-        <div className="housing-index">
-            <h1>Stays in City One: </h1>
-            {this.props.housings.map(housing => (
-                <div className="housing-element" key={housing.id} onClick={() => this.handleClick(housing.id)}>
-                    <img src={housing.photoUrl} />
-                    <div>
-                        <p>{housing.housing_type} in {housing.address}</p>
-                        <p id="housing-name" >{housing.name}</p>
-                        <p>{housing.guests} guests  {housing.bedrooms} bedroom/s {housing.beds} bed/s {housing.baths} bath/s</p>
-                        <p id="housing-price">${housing.price} / night</p>
+        <div className="housing-index-page">
+            <div className="housing-index">
+                <h1>Stays in City One: </h1>
+                {this.props.housings.map(housing => (
+                    <div className="housing-element" key={housing.id} onClick={() => this.handleClick(housing.id)}>
+                        <img src={housing.photoUrl} />
+                        <div>
+                            <p>{housing.housing_type} in {housing.address}</p>
+                            <p id="housing-name" >{housing.name}</p>
+                            <p>{housing.guests} guests  {housing.bedrooms} bedroom/s {housing.beds} bed/s {housing.baths} bath/s</p>
+                            <p id="housing-price">${housing.price} / night</p>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
+            <div id="map"></div>
         </div>
     )}
 };
